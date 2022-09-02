a926de2f116dec7c4b0a58fe6715003ee5c13f9b
markFT39 <43339545+markFT39@users.noreply.github.com>
2022-09-02 11:04:14 +0900

    2nd commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world, 안녕!");
+		new WordManager().start();
 	}
-
 }
