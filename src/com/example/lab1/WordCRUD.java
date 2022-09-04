e54436b9bbc6956b8dc3586693ebdbe9144f5418
markFT39 <43339545+markFT39@users.noreply.github.com>
2022-09-05 02:31:10 +0900

    commit test
    commit form github desktop

--- src/com/example/lab1/WordCRUD.java
+++ src/com/example/lab1/WordCRUD.java
@@ -48,11 +48,11 @@ public class WordCRUD implements ICRUD {
 		
 	}
 	public void listAll() {
-		System.out.println("\n--------------------------------------");
+		System.out.println("\n---------------------------------------");
 		for(int i = 0; i < list.size(); i++) {
 			System.out.print((i+1) + " ");
 			System.out.println(list.get(i).toString());
 		}
-		System.out.println("--------------------------------------\n");
+		System.out.println("---------------------------------------\n");
 	}
 }
